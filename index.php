8d13c9efe75a54adecc393505e0ba9b106f92f57
leewalling <lwalling@USNY-kchandler.lbi.local>
2013-08-23 13:32:19 -0400

    fixed nav

--- index.php
+++ index.php
@@ -16,16 +16,58 @@
 	</head>
 	<body>
 
-	<div class="container">
-		<div class="row">
-			<div class="sidebar col-lg-3 col-sm-2 col-2 well">
-				<h1>Left side</h1>
-			</div>
-			<div class="content col-lg-9 col-sm-10 col-10 well">
-				<p class="lead">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Assumenda, vitae, eum, voluptatibus laboriosam voluptates non corporis iste soluta porro eaque dicta ipsum fugit expedita provident iusto nostrum odit repellat dignissimos.</p>
-			</div>
-		</div>
-	</div>
+<!-- Fixed navbar -->
+    <div class="navbar navbar-default navbar-fixed-top">
+      <div class="container">
+        <div class="navbar-header">
+          <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+          </button>
+          <a class="navbar-brand" href="#">Project name</a>
+        </div>
+        <div class="navbar-collapse collapse">
+          <ul class="nav navbar-nav">
+            <li class="active"><a href="#">Home</a></li>
+            <li><a href="#about">About</a></li>
+            <li><a href="#contact">Contact</a></li>
+            <li class="dropdown">
+              <a href="#" class="dropdown-toggle" data-toggle="dropdown">Dropdown <b class="caret"></b></a>
+              <ul class="dropdown-menu">
+                <li><a href="#">Action</a></li>
+                <li><a href="#">Another action</a></li>
+                <li><a href="#">Something else here</a></li>
+                <li class="divider"></li>
+                <li class="dropdown-header">Nav header</li>
+                <li><a href="#">Separated link</a></li>
+                <li><a href="#">One more separated link</a></li>
+              </ul>
+            </li>
+          </ul>
+          <ul class="nav navbar-nav navbar-right">
+            <li><a href="../navbar/">Default</a></li>
+            <li><a href="../navbar-static-top/">Static top</a></li>
+            <li class="active"><a href="./">Fixed top</a></li>
+          </ul>
+        </div><!--/.nav-collapse -->
+      </div>
+    </div>
+
+    <div class="container">
+
+      <!-- Main component for a primary marketing message or call to action -->
+      <div class="jumbotron">
+        <h1>Navbar example</h1>
+        <p>This example is a quick exercise to illustrate how the default, static and fixed to top navbar work. It includes the responsive CSS and HTML, so it also adapts to your viewport and device.</p>
+        <p>To see the difference between static and fixed top navbars, just scroll.</p>
+        <p>
+          <a class="btn btn-lg btn-primary" href="../../components/#navbar">View navbar docs »</a>
+        </p>
+      </div>
+
+    </div> <!-- /container -->
+
 		<!--div id="hpit">
 			<div class="main-header">header</div>
 			<div class="main-content">main-content</div>
