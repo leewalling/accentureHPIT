11e74a4042e99d7f33759c01455cedbe14947f52
leewalling <lwalling@USNY-kchandler.lbi.local>
2013-08-24 21:06:59 -0400

    js

--- js/hpit.js
+++ js/hpit.js
@@ -44,13 +44,107 @@ hpit.core = (function(){
 		resetSite();
 				
 		// Initialize event handler for 'Back to top' links
-		backToTopInit();
+		//backToTopInit();
 		
 		// Work out whether IE7 is being used to view the site, due to the way absolute vs. fixed positioning works differently in that browser
 		hpit.configuration.isIE7 = navigator.userAgent.toLowerCase().indexOf("msie 7.") != -1;
 		
 		var validHashValue = false;
+
+		console.log('config: ', hpit.configuration);
+
+		$('.insight').each(function () {
+			lockscroll($(this));
+		});
 	}
+
+	function lockscroll(element) {
+
+	    $(window)
+	        .bind('scroll', function () {
+				console.log('ele: ', element);
+	            
+	            /*
+	            //The module we're currently working with
+	            var selectedModule = element;
+
+	            //The static column we're currently working with
+	            var selectedCol = element.find('.static_col');
+
+	            if (selectedCol.length <= 0) {
+	                return false;
+	            }
+
+	            var parOffset = selectedCol.parent()
+	                .offset();
+	            parOffset = parOffset.left;
+
+	            //Lowest value the margin can be is zero.
+	            var startpoint = 0;
+
+	            //Viewport points
+	            var viewportBegin = selectedModule.offset()
+	                .top - 65
+	            var viewportEnd = viewportBegin + selectedModule.height();
+
+	            //Stop point will always be the height difference between the static column and it's parent.
+	            //Essentially, that's the maximum value the margin can be
+	            var stopPoint = (selectedModule.height() - selectedCol.height());
+
+	            //How much of the page have we scrolled?
+	            var winY = $(window)
+	                .scrollTop();
+
+	            //Do some adjustments if our module is within the viewport
+	            if (winY >= viewportBegin && winY < viewportEnd) {
+	                //fix our static column to the bottom of the module if we've scroll below our viewport
+	                if ((winY - viewportBegin) >= stopPoint) {
+	                    selectedCol.removeClass('fixed')
+	                        .css({
+	                            'bottom': '',
+	                            'top': stopPoint,
+	                            'left': 0
+	                        });
+	                }
+	                //fix our static column to the top of the module if we've scrolled above our viewport
+	                else if ((winY - viewportBegin) <= 0) {
+	                    selectedCol.removeClass('fixed')
+	                        .css({
+	                            'top': 0,
+	                            'bottom': '',
+	                            'left': 0
+	                        });
+	                }
+	                //Animate the margin while scrolling within the viewport
+	                else {
+	                    var offset = (winY - viewportBegin);
+
+	                    //console.log("winY: " + winY + ", viewportBegin: " + viewportBegin + ", viewportEnd: " + viewportEnd + ", stopPoint: " + stopPoint + ", startPoint: " + startpoint + ', offset: ' + offset);
+
+	                    selectedCol.addClass('fixed')
+	                        .css({
+	                            'top': 65,
+	                            'bottom': '',
+	                            'left': parOffset
+	                        });
+	                }
+
+	            }
+	            //We're out of the viewport so lock the column back to it's start point
+	            else {
+	                selectedCol.removeClass('fixed')
+	                    .css({
+	                        'top': 0,
+	                        'bottom': '',
+	                        'left': 0
+	                    });
+	            }
+
+	            */
+
+	        }); //End Scroll Event
+
+	}; //End Function
 
 	/**
 	* Calcaulate/re-calculate any dimensions which may be altered by an orientation change or browser resize
