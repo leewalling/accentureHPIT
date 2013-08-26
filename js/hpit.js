0335658cff533e66fba34a4a87a1e44e1ecf46ee
leewalling <lwalling@USNY-kchandler.lbi.local>
2013-08-26 13:04:58 -0400

    js

--- js/hpit.js
+++ js/hpit.js
@@ -108,8 +108,8 @@ hpit.core = (function(){
 	
 	// sidemenu toggle method
 	function toggleMenuInit(){
-		console.log('available height: ' + hpit.configuration.screenHeight - 62);
-		console.log('menu height: ' + $('#sideMenu').outerHeight());
+		console.log('available height: ' + parseInt(hpit.configuration.screenHeight) - 62);
+		console.log('menu height: ' + $('#sideMenu > ul').outerHeight());
 		
 		var $target = $('#sideMenu');
 		$target.css({"height" : hpit.configuration.screenHeight - 62});
