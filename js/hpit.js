6828008626361821c08e392660d3a5f2e5f8abe1
Lee Walling <lee.walling@mry.com>
2013-09-03 18:11:31 -0400

    responsive

--- js/hpit.js
+++ js/hpit.js
@@ -241,14 +241,11 @@ hpit.core = (function(){
 			var $trgt = $th.parent().parent();
 			var $cont = $trgt.find('.container.content');
 			if($trgt.hasClass('open')){
-				$cont.slideUp(1000, function() {
-					$trgt.removeClass('open');
-				});
+				$cont.slideUp(500);
+				$trgt.removeClass('open');
 			} else {
-				//$trgt.addClass('open');
-				$cont.slideDown(1000, function() {
-					$trgt.addClass('open');
-				});
+				$trgt.addClass('open');
+				$cont.slideDown(500);
 			}
 		});
 	}
