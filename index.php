b61c88104757ef70bd613bbbd48f33068f2005cb
Lee Walling <lee.walling@mry.com>
2013-09-19 09:32:45 -0400

    design feedback

--- index.php
+++ index.php
@@ -23,9 +23,9 @@
 	<div id="ll-overlay">
 		<div class="inner">
 			<div id="ll-player">
-				<script src="https://assets.delvenetworks.com/player/embed.js"></script>
-				<object type="application/x-shockwave-flash" id="limelight_player_239897" name="limelight_player_239897" class="LimelightEmbeddedPlayerFlash" width="100%" height="100%" data="https://assets.delvenetworks.com/player/loader.swf">
-					<param name="movie" value="https://assets.delvenetworks.com/player/loader.swf"/>
+				<script src="//assets.delvenetworks.com/player/embed.js"></script>
+				<object type="application/x-shockwave-flash" id="limelight_player_239897" name="limelight_player_239897" class="LimelightEmbeddedPlayerFlash" width="100%" height="100%" data="//assets.delvenetworks.com/player/loader.swf">
+					<param name="movie" value="//assets.delvenetworks.com/player/loader.swf"/>
 					<param name="wmode" value="transparent"/>
 					<param name="allowScriptAccess" value="always"/>
 					<param name="allowFullScreen" value="true"/>
