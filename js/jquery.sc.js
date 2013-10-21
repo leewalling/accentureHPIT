5c2574df1cc6a1546659aeaa074543306d2b545c
Adam Balchunas <adam.balchunas@digitas.com>
2013-10-20 22:12:30 -0400

    Reverted jquery.sc.js

--- js/jquery.sc.js
+++ js/jquery.sc.js
@@ -2,10 +2,9 @@
  * Selector Cache $$
  * Cache your selectors, without messy code.
  * @author Stephen Kamenar
- * @author Adam Balchunas
  */
 (function ($) {
-	$$ = function(selector, action, context) {
+	$$ = function(selector, action) {
 		var that = this;
 		//Set the cache to empty object right away
 		if(that.init === undefined) {
@@ -22,8 +21,7 @@
 		//entry point
 		switch(action) {
 			case 'get': return selector_get(); break;
-         case 'context-get': return selector_context_get(context); break;
-         case 'clear': return selector_clear(); break;
+			case 'clear': return selector_clear(); break;
 			case 'fresh': return selector_fresh(); break;
 			default: throw new Error('Invalid action passed to jQuery Selector Cache');
 		}
@@ -32,28 +30,15 @@
 			//check if the selector is in the cache
 			if(that.c[selector] !== undefined) {
 				return that.c[selector];
-			} 
-         else {
-            return that.c[selector] = $(selector);
-         }
+			} else {
+				return that.c[selector] = $(selector);
+			}
 		}
-
-      function selector_context_get() {
-      	//check if the selector is in the cache
-			if(that.c[selector] !== undefined) {
-				return that.c[selector];
-			} 
-         else {
-            return that.c[selector] = $(selector, context);
-         }
-      }
-
 		function selector_clear() {
 			var temp = that.c[selector];
 			delete that.c[selector];
 			return temp;
 		}
-
 		function selector_fresh() {
 			selector_clear();
 			return selector_get();
