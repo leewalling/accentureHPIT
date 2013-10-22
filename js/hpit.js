ad56ee2f807fb0094a29bbad08e9d9979ae27719
Lee Walling <lee.walling@mry.com>
2013-10-22 18:45:06 -0400

    galaxy updates

--- js/hpit.js
+++ js/hpit.js
@@ -487,7 +487,7 @@ hpit.core = (function() {
             paneLock();
         }
         
-        if (onMobile()) {
+        if (onMobile() && !onGalaxyTab()) {
             arrangeTopNav();
         }
 
